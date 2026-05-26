7cfeb5dd6f25970d3c8dfcce8ffc830aaf123fae
Elmer-Fuentes <delacruzelmer27@gmail.com>
2026-05-26 17:32:49 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOpsConContenedoresDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
